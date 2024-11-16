8fb514dd2c5fb031024150a61b892ce12dfe2add
lina <lina.kuhn@ymail.com>
2024-11-17 00:57:11 +0100

    add submit button for feedback

--- src/routes/FeedbackForm.svelte
+++ src/routes/FeedbackForm.svelte
@@ -23,5 +23,8 @@
 			<label for="zusatz" class="block mb-2 text-sm font-medium text-gray-900">Was ich euch noch sagen moechte</label>
 			<input type="text" id="zusatz" class="block w-full p-2 text-gray-900 border border-gray-300 rounded-lg bg-gray-50 text-xs focus:ring-blue-500 focus:border-blue-500">
 		</div>
+		<div class="py-5">
+			<button type="submit" class="text-white bg-slate-600 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center">Feedback Abschicken</button>
+		</div>
 	</form>
 </div>
--- src/routes/pralinen/+page.svelte
+++ src/routes/pralinen/+page.svelte
@@ -3,7 +3,18 @@
 	import praline_demo_1 from '$lib/assets/2023/praline_demo_1.jpeg'
 </script>
 
+<div class="text-lg font-bold">
+	<h1 class="text-regular">Kollektion 2024</h1>
+</div>
 <div class="px-20 py-20 fle align-middle justify-items-center">
 		<Praline picture={praline_demo_1} name='Karamell Meersalz' description='Ein Traum aus Karamell und Meersalz' />
 		<Praline picture={praline_demo_1} name='Karamell Meersalz' description='Ein Traum aus Karamell und Meersalz' />
 </div>
+
+<style>
+    .text-regular {
+        padding-top: 100px;
+        text-align: center;
+        font-size: xx-large;
+    }
+</style>
