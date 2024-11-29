d5a6fb6a31c223a575ea07e395879167dfde3271
lina <lina.kuhn@ymail.com>
2024-11-29 18:34:29 +0100

    add philosophie page

---
+++ src/routes/philosophie/+page.svelte
@@ -0,0 +1,15 @@
+<script>
+	import schokolade from'$lib/assets/2023/schokolade.png';
+</script>
+
+
+<div class="flex flex-col items-center p-10">
+	<h1 class="great-vibes-regular">Philosophie</h1>
+	<img src={schokolade} alt="making-of" title="making-of" class="w-1/4">
+</div>
+
+<style>
+	.great-vibes-regular {
+			font-size: xxx-large;
+	}
+</style>
