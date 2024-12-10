ac8e8c3677868312374013faeb189b3d75cef099
lina <lina.kuhn@ymail.com>
2024-12-10 21:34:28 +0100

    add making of images and page

---
+++ src/components/RoundedImageWithDescription.svelte
@@ -0,0 +1,12 @@
+<script>
+	let {image = '', alt = '', description = '', imagePosition = 'left' } = $props()
+</script>
+<div class="flex flex-row items-center justify-center gap-10 py-5">
+	{#if imagePosition === 'left'}
+		<p>{description}</p>
+	{/if}
+	<img class="object-cover rounded-lg md:h-auto w-3/4 hover-zoom" src={image} alt={alt}>
+	{#if imagePosition === 'right'}
+		<p>{description}</p>
+	{/if}
+</div>
--- src/routes/makingof/+page.svelte
+++ src/routes/makingof/+page.svelte
@@ -1,11 +1,16 @@
 <script>
-	// import schokolade from'$lib/assets/2023/schokolade.png';
+	import schokolade from'$lib/assets/makingof/schokolade_small.png';
+	import gruppenfoto_23 from '$lib/assets/makingof/gruppenfoto_23_small.png';
+	import makingof_23 from '$lib/assets/makingof/makingof_23_small.png';
+	import RoundedImageWithDescription from '../../components/RoundedImageWithDescription.svelte';
 </script>
 
 
-<div class="flex flex-col items-center p-10">
-	<h1 class="great-vibes-regular">Making Of</h1>
-<!--	<img src={schokolade} alt="making-of" title="making-of" class="w-1/4">-->
+<div class="flex flex-col items-center p-5 gap-20">
+	<h1 class="great-vibes-regular pb-5">Making Of</h1>
+		<RoundedImageWithDescription image={schokolade} alt={'schokolade'} imagePosition={'left'} description="schokolade" />
+		<RoundedImageWithDescription image={gruppenfoto_23} alt={'gruppenfoto 23'} imagePosition={'right'} description="gruppenfoto 23" />
+		<RoundedImageWithDescription image={makingof_23} alt={'makingof 23'} imagePosition={'left'} description="making of" />
 </div>
 
 <style>
