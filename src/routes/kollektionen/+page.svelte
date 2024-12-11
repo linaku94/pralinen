78e854be84d2fb4879ff8d1fc6633f9cfffa35de
lina <lina.kuhn@ymail.com>
2024-12-11 11:41:37 +0100

    fix padding

--- src/routes/kollektionen/+page.svelte
+++ src/routes/kollektionen/+page.svelte
@@ -4,7 +4,7 @@
 
 <div class="text-lg font-bold">
 	<h1 class="great-vibes-regular pt-10">Vergangene Kollektionen</h1>
-	<div class="flex justify-center md:flex-row flex-col p-10 md:gap-32 gap-16">
+	<div class="flex justify-center md:flex-row flex-col p-7 md:gap-32 gap-16">
 		<CollectionOverview year="2021" bgvalue="bg-himbeer"/>
 		<CollectionOverview year="2022" bgvalue="bg-zitrone22"/>
 		<CollectionOverview year="2023" bgvalue="bg-karamell23"/>
