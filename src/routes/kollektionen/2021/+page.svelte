0cce323d9cc3022b1209250253887fc83090d87c
lina <lina.kuhn@ymail.com>
2024-12-14 10:43:34 +0100

    styling adaptions of texts

--- src/routes/kollektionen/2021/+page.svelte
+++ src/routes/kollektionen/2021/+page.svelte
@@ -10,7 +10,7 @@
 
 <div class="bg-mango bg-cover bg-center">
 	<div class="text-lg font-bold">
-		<h1 class="great-vibes-regular text-center text-2xl pt-10">Kollektion 2021</h1>
+		<h1 class="great-vibes-regular text-center text-3xl pt-10">Kollektion 2021</h1>
 	</div>
 	<div class="px-10 py-10 flex flex-col items-center">
 		<Praline picture={Himbeer} name='Himbeer' description='Unser zeitloser Superstar' />
