402b347f43667daa402f32a027ced0665a4c477a
lina <lina.kuhn@ymail.com>
2024-12-14 12:20:19 +0100

    remove pralinen component dark mode

--- src/components/Praline.svelte
+++ src/components/Praline.svelte
@@ -4,11 +4,11 @@ export let name = 'Praline';
 export let description = 'Lecker Praline mhmmmm'
 </script>
 <div class="px-10 py-10 text-chocolate-dark">
-	<div class="hover-zoom flex flex-col md:flex-row items-center bg-chocolate-light dark:bg-chocolate rounded-lg shadow">
+	<div class="hover-zoom flex flex-col md:flex-row items-center bg-chocolate-light rounded-lg shadow">
 		<img class="object-cover self-center rounded-t-lg md:h-auto md:w-48 md:rounded-none md:rounded-s-lg" src={picture} alt="praline demo 1">
 		<div class="flex flex-col justify-between p-4 leading-normal">
-			<h5 class="mb-2 text-2xl font-bold tracking-tight dark:text-chocolate-white">{name}</h5>
-			<p class="mb-3 font-normal dark:text-chocolate-white">{description}</p>
+			<h5 class="mb-2 text-2xl font-bold tracking-tight">{name}</h5>
+			<p class="mb-3 font-normal">{description}</p>
 		</div>
 	</div>
 </div>
