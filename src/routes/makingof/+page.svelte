4e918c71264399dd1c4df1a637c895f428b4d48c
lina <lina.kuhn@ymail.com>
2024-12-15 16:17:46 +0100

    making of 24

--- src/routes/makingof/+page.svelte
+++ src/routes/makingof/+page.svelte
@@ -1,17 +1,20 @@
 <script>
 	import schokolade from'$lib/assets/makingof/schokolade_small.png';
-	import gruppenfoto_23 from '$lib/assets/makingof/gruppenfoto_23_small.png';
-	import makingof_23 from '$lib/assets/makingof/makingof_23_small.png';
+	import arbeiten2 from '$lib/assets/makingof/arbeiten2.png';
+	import arbeiten from '$lib/assets/makingof/arbeiten.png';
+	import gruppenbild from '$lib/assets/makingof/karamell_gruppenbild.png';
+	import ergebnis from '$lib/assets/makingof/ergebnis.png';
 	import RoundedImageWithDescription from '../../components/RoundedImageWithDescription.svelte';
 </script>
 
 
 <div class="flex flex-col items-center justify-center gap-16 md:gap-32">
-	<h1 class="great-vibes-regular text-3xl pt-5">Making Of</h1>
+	<h1 class="great-vibes-regular text-3xl pt-10">Making Of</h1>
 		<RoundedImageWithDescription image={schokolade} alt={'schokolade'} imagePosition={'left'} description="Hier beginnt alles mit 5kg feinster Schokolade..." />
-		<RoundedImageWithDescription image={gruppenfoto_23} alt={'gruppenfoto 23'} imagePosition={'right'} description="... die zu köstlichen Füllungen verarbeitet wird ..." />
-		<RoundedImageWithDescription image={makingof_23} alt={'makingof 23'} imagePosition={'left'} description="... die Pralinen werden befüllt, versiegelt ..." />
-		<RoundedImageWithDescription image={makingof_23} alt={'makingof 23'} imagePosition={'left'} description="... ummantelt und verziert ..." />
+		<RoundedImageWithDescription image={gruppenbild} alt={'gruppenbild'} imagePosition={'right'} description="... die zu köstlichen Füllungen verarbeitet wird ..." />
+		<RoundedImageWithDescription image={arbeiten2} alt={'arbeiten'} imagePosition={'left'} description="... die Pralinen werden befüllt, versiegelt ..." />
+		<RoundedImageWithDescription image={arbeiten} alt={'arbeiten2'} imagePosition={'right'} description="... ummantelt und verziert ..." />
+		<RoundedImageWithDescription image={ergebnis} alt={'ergebnis'} imagePosition={'left'} description="... bis alle fertig sind." />
 </div>
 
 <style>
