a50f96a73db6ab9aacc3771ce721ea293aadbac4
lina <lina.kuhn@ymail.com>
2024-12-17 18:25:11 +0100

    remove tailwindforms and daisy ui

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,6 +1,4 @@
-import forms from '@tailwindcss/forms';
 import type { Config } from 'tailwindcss';
-import daisyui from 'daisyui';
 
 export default {
 	content: ['./src/**/*.{html,js,svelte,ts}'],
@@ -15,13 +13,13 @@ export default {
 				'chocolate-caramel': '#FFD29A'
 			},
 			backgroundImage: {
-				'mango': 'url($lib/assets/2021/mango_medium.png)',
-				'himbeer' : 'url($lib/assets/2021/himbeer_medium.png)',
-				'zitrone' : 'url($lib/assets/2021/zitrone_medium.png)',
-				'karamell23' : 'url($lib/assets/2023/karamell_medium.png)',
-				'zitrone22' : 'url($lib/assets/2022/zitrone_medium.png)',
-				'karlsruhe24' : 'url($lib/assets/2024/karlsruhe_hd.png)',
+				mango: 'url($lib/assets/2021/mango_medium.png)',
+				himbeer: 'url($lib/assets/2021/himbeer_medium.png)',
+				zitrone: 'url($lib/assets/2021/zitrone_medium.png)',
+				karamell23: 'url($lib/assets/2023/karamell_medium.png)',
+				zitrone22: 'url($lib/assets/2022/zitrone_medium.png)',
+				karlsruhe24: 'url($lib/assets/2024/karlsruhe_hd.png)'
 			}
 		}
-	},
+	}
 } satisfies Config;
