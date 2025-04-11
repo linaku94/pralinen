4b4b238a0ee127eb69e94a86c5caee957535666d
lina <lina.kuhn@ymail.com>
2025-04-11 17:00:31 +0200

    add svelte-ux
    which seems to be required to make layer chart working. Maybe its also just the layerchart plugin, so may be possible to remove svelte-ux again.

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,7 +1,13 @@
 import type { Config } from 'tailwindcss';
+import colors from 'tailwindcss/colors';
+import layerstack from '@layerstack/tailwind/plugin'
 
 export default {
-	content: ['./src/**/*.{html,js,svelte,ts}', './node_modules/layerchart/**/*.{svelte,js}'],
+	content: [
+		'./src/**/*.{html,js,svelte,ts}',
+		'./node_modules/svelte-ux/**/*.{svelte,js}',
+		'./node_modules/layerchart/**/*.{svelte,js}'
+	],
 
 	theme: {
 		extend: {
@@ -21,5 +27,34 @@ export default {
 				karlsruhe24: 'url($lib/assets/2024/karlsruhe_hd.png)'
 			}
 		}
-	}
+	},
+
+	ux: {
+		themes: {
+			light: {
+				primary: colors['orange']['500'],
+				'primary-content': 'white',
+				secondary: colors['blue']['500'],
+				'surface-100': 'white',
+				'surface-200': colors['gray']['100'],
+				'surface-300': colors['gray']['300'],
+				'surface-content': colors['gray']['900'],
+				'color-scheme': 'light'
+			},
+			dark: {
+				primary: colors['orange']['500'],
+				'primary-content': 'white',
+				secondary: colors['blue']['500'],
+				'surface-100': colors['zinc']['800'],
+				'surface-200': colors['zinc']['900'],
+				'surface-300': colors['zinc']['950'],
+				'surface-content': colors['zinc']['100'],
+				'color-scheme': 'dark'
+			}
+		}
+	},
+
+	plugins: [
+		layerstack,  // uses hsl() color space by default. To use oklch(), use: layerstack({ colorSpace: 'oklch' }),
+	]
 } satisfies Config;
