1a840773624f5eb9b4c6ad664121e0fe7e9188ed
lina <lina.kuhn@ymail.com>
2025-04-25 10:21:30 +0200

    update tailwind css to v4
    using the upgrade tool and some minor manual adjustments.

--- src/components/FormComponents/SubmitButton.svelte
+++ src/components/FormComponents/SubmitButton.svelte
@@ -4,7 +4,7 @@
 
 <div class="py-5">
 	<button type="submit"
-					class="text-chocolate-white bg-chocolate-dark hover:bg-chocolate-light focus:ring-4 focus:outline-none focus:ring-chocolate-light font-medium rounded-lg text-sm px-5 py-2.5 text-center">
+					class="text-chocolate-white bg-chocolate-dark hover:bg-chocolate-light focus:ring-4 focus:outline-hidden focus:ring-chocolate-light font-medium rounded-lg text-sm px-5 py-2.5 text-center">
 		{description}
 	</button>
 </div>
